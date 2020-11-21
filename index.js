75e91a4556c8c7be9f79246d838bfdb013823dbd
softwaree40 <softwaree40@gmail.com>
2020-11-21 02:51:15 -0500

    splitting the str to array to get the first word converted

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ const oneWord = function(str){
  return str.replace(/ /g,"").toLowerCase()
 
 }
+// upper the first word
+const upperTheFirstWord = function(str){
 
+   // convert the string to array by splitting each of the word
+   let splitWord = str.split(" ")
+   // destruction the word into different variable 
+   const [first,...others] = splitWord;
+   //convert the first word in that variable (first)to uppercase and join back to string
+   return [first.toUpperCase(),...others].join(" ");
+
+}
