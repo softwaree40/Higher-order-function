b3d312547bcf6b0a3d81ac16761dcee39cd9e9fe
softwaree40 <softwaree40@gmail.com>
2020-11-21 03:10:55 -0500

    adding html to my code to view all code functionality

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-"use strict"
+ "use strict"
 // oneWord replace all space
 const oneWord = function(str){
 
- return str.replace(/ /g,"").toLowerCase()
+ return str.replace(/ /g,"").toLowerCase();
 
-}
+};
 // upper the first word
 const upperTheFirstWord = function(str){
 
    // convert the string to array by splitting each of the word
-   let splitWord = str.split(" ")
+   let splitWord = str.split(" ");
    // destruction the word into different variable 
    const [first,...others] = splitWord;
    //convert the first word in that variable (first)to uppercase and join back to string
    return [first.toUpperCase(),...others].join(" ");
 
+};
+// creating higher order function that take the str arg and function
+const higherOrderFunction = function(str, fx) {
+  console.log(`This is original string: ${str} new transform string ${fx(str)}`)
+
 }
+higherOrderFunction("JavaScript is the best Language!",oneWord)
