8161052f3356979e16405e55e82a30c89a8127e4
softwaree40 <softwaree40@gmail.com>
2020-11-21 05:01:13 -0500

    using arrow function to do same function return function

--- index.js
+++ index.js
@@ -16,9 +16,50 @@ const upperTheFirstWord = function(str){
    return [first.toUpperCase(),...others].join(" ");
 
 };
+const capitalizeFirstLetter = function(str){
+     let result = []
+    // split the word example: welcome to school => ["welcome","to","school"]
+    let splitWordNow = str.split(" ")
+    for(const eachWord of splitWordNow){
+
+       const [firstWord,...others] = eachWord
+      result.push(firstWord.toUpperCase() + others.join(""))
+    
+    }
+    return result
+}
 // creating higher order function that take the str arg and function
 const higherOrderFunction = function(str, fx) {
-  console.log(`This is original string: ${str} new transform string ${fx(str)}`)
+  console.log(`This is original string: ${str}`)
+  console.log(`Transform original string: ${fx(str)}`)
+  console.log(`What is my function Name: ${fx.name}`)
+  console.log(`We need use transformer function ${fx(str)}`)
+}
+higherOrderFunction("javaScript is the best language!",capitalizeFirstLetter)
+higherOrderFunction("we are back to school",capitalizeFirstLetter)
+//Writting functions that return another functions
+
+// const greeting = function(greet){
+
+//     return function(name){
+
+//        console.log(`${greet} ${name}`)
+//        console.log(`${greeting.name}`)
+
+//     }
+    
+
+// }
+
+// Using greeting function as arrow function
+
+const greetingArrow = greet => name => {
+
+    return (console.log(`${greet} ${name}`),
+            console.log(`${greetingArrow.name}`)
+     )
+    
 
 }
-higherOrderFunction("JavaScript is the best Language!",oneWord)
+
+greetingArrow("Welcome")("Wale")
